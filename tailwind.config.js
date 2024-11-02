b2a3328dd7718a3abcef116d717077023cb717e5
Steve Mogan Odumbe <124534650+StevetheRebel@users.noreply.github.com>
2024-11-03 01:33:57 +0300

    add

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-blue': "hsl(233, 26%, 24%)",
+        'lime-green': "hsl(136, 65%, 51%)",
+        'bright-cyan': "hsl(192, 70%, 51%)",
+        'neutral-grayish-blue': "hsl(233, 8%, 62%)",
+        'neutral-light-grayish-blue': "hsl(220, 16%, 96%)",
+        'neutral-very-light-gray': "hsl(0, 0%, 98%)",
+        'neutral-white': "hsl(0, 0%, 100%)"
+      },
+      fontFamily: {
+        sans: ['"Public Sans"', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
