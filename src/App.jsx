7b7ab9cdef89cd05a5454a93e7303860a8f367cc
Steve Mogan Odumbe <124534650+StevetheRebel@users.noreply.github.com>
2024-11-03 17:23:28 +0300

    section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navigation from './components/Navigation'
 import Hero from './components/Hero'
+import Choose from './components/Choose'
 
 function App() {
   return (
     <>
     <Navigation />
     <Hero />
+    <Choose />
     </>
   )
 }
