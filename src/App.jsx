c989945db93f026db8733d2ab3508ec115f6b8a5
Steve Mogan Odumbe <124534650+StevetheRebel@users.noreply.github.com>
2024-11-03 21:12:49 +0300

    footer component added and navigation edited

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navigation from './components/Navigation'
 import Hero from './components/Hero'
 import Choose from './components/Choose'
 import Articles from './components/Articles'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <Hero />
     <Choose />
     <Articles />
+    <Footer />
     </>
   )
 }
